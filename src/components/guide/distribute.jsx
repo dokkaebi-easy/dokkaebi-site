c51a6e9adf751e8334a21eae9977a89b6d572860
ethankim93 <wsmh.kim12@gmail.com>
2022-05-12 11:57:06 +0900

    fear : loginDeploy

--- src/components/guide/distribute.jsx
+++ src/components/guide/distribute.jsx
@@ -1,10 +1,61 @@
+import styled from "@emotion/styled";
 const Distribute = () => {
   return (
     <div>
-      <h1>프로젝트 배포 페이지</h1>
-      <p>프로젝트 배포 페이지</p>
+      <H1>프로젝트 배포</H1>
+      <h2>프로젝트 배포</h2>
+      <br></br>
+      <P1>- 프로젝트 build setting이 끝나면 메인화면으로 이동</P1>
+      <P1>- `빌드실행`을 버튼을 클릭해 빌드/배포 해주세요.</P1>
+      <img src="img/guide/deploy/01.png" alt="01" />
+      <br></br>
+      <br></br>
+      <br></br>
+      <Textbox>
+      📢 화면에 보이는 S, 최근성공, 최근실패, 최근 소요시간 의 view는 개발 진행중에 있습니다.
+      <br />
+     
+      추후 업데이트 후 안내드리겠습니다
+      </Textbox>
+      <br />
+      <br />npm
+      <h2>프로젝트 상세페이지</h2>
+      <P1>- Main의 프로젝트 목록에서 Project ID 또는 Name을 클릭하면 빌드 상세화면에 접속할 수 있습니다.</P1>
+      <P1>- 배포 진행 과정은 Sate List를 통해 **Pull, Build, Run** 상태를 확인할 수 있습니다.</P1>
+
+      <img src="img/guide/deploy/02.png" alt="02" />
+      <Textbox>
+      📢 화면에 실시간으로 빌드 현황을 보여주는 Stage View는 개발 진행중에 있습니다.
+      <br />
+      추후 업데이트 후 안내드리겠습니다.
+      </Textbox>
+
     </div>
   );
 };
-
 export default Distribute;
+
+const H1 = styled.h1`
+  background-color: 1976D2;
+  color: white;
+  padding-left: 10px;
+  padding-top: 10px;
+  padding-bottom: 10px;
+`;
+
+const Textbox = styled.div`
+  border: 1px solid black;
+  border-color: D9D9D9;
+  padding: 10px;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  margin-right: 16px;
+  background-color : CCFFFF;
+  color: black;
+  font-size: 18px;
+`;
+
+const P1 = styled.p`
+  font-size: 20px;
+  margin-top: 10px;
+`;
--- src/components/guide/login.jsx
+++ src/components/guide/login.jsx
@@ -3,10 +3,66 @@ import styled from "@emotion/styled";
 const Login = () => {
   return (
     <div>
-      <h1>Dockerby 회원가입</h1>
-      <p>회원가입 / 로그인 페이지</p>
+      <H1>회원가입 / 로그인</H1>
+      <h2>Dockerby 회원가입</h2>
+      <br></br>
+
+      <P1>1. 가입하기 버튼을 클릭→ 회원가입 창으로 이동</P1>
+      <img src="img/guide/login/01.png" alt="login_view" />
+      <br></br>
+      <br></br>
+      <br></br>
+
+      <P1>2. 회원가입 FROM 양식에 맞춰서 기입</P1>
+      <img src="img/guide/login/02.png" alt="signup_view" />
+
+      <P1>- ID 중복 확인, 이름 중복 확인을 통해 validation 체크해주세요.</P1>
+      <P1>- 인증키는 여러분의 프로젝트에 다른 사람으로부터 접근을 보호하기 위한 수단입니다.</P1>
+      <P1> dockerby 설치 후, 아래 명령어를 통해 출력된 AuthKey를 입력해주세요.</P1>
+
+      <Textbox>
+        # dockerby 컨테이너에 접속
+        <br></br>
+        $ docker exec -it dockerby bash
+        <br></br>
+        <br></br>
+
+        # AuthKey출력 
+        <br></br>
+        $ cat AuthKey 
+        <br></br>
+        # 7c8c7b0a5a74482e9e9ba84444cc0f12MDIyOTU5Cg
+      </Textbox>
+      <br></br>
+
+      <P1>3. Dockerby 로그인</P1>
+      <img src="img/guide/login/03.png" alt="login_view2" />
+
     </div>
   );
 };
-
 export default Login;
+
+const H1 = styled.h1`
+  background-color: 1976D2;
+  color: white;
+  padding-left: 10px;
+  padding-top: 10px;
+  padding-bottom: 10px;
+`;
+
+const Textbox = styled.div`
+  border: 1px solid black;
+  border-color: D9D9D9;
+  padding: 10px;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  margin-right: 16px;
+  color: 000099;
+  font-size: 18px;
+`;
+
+const P1 = styled.p`
+  font-size: 20px;
+  margin-top: 10px;
+`;
