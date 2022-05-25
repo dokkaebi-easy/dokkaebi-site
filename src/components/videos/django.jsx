5a3b0ed31eec63825acfee9203d4c5bbe982442b
beck <backcy1@daum.net>
2022-05-25 16:25:04 +0900

    장고시연 영상 업로드

--- src/components/videos/django.jsx
+++ src/components/videos/django.jsx
@@ -6,15 +6,17 @@ const Django = () => {
   return (
     <div>
       <Videonav />
-      <ItemCenter>
-        <ConstructionIcon sx={{ fontSize: "80px" }} />
-        <MainMassage>동영상을 업데이트 중 입니다.</MainMassage>
-        <p align="center">
-          더 쉬운 사용 방법을 알려드리기 위해 고민중 입니다. <br /> 빠른 시일 내
-          작업을 완료하고, 새로운 모습으로 찾아 뵙겠습니다.
-          <br /> we're busy updating our website for you and will be back soon
-        </p>
-      </ItemCenter>
+      <IframeDiv>
+        <iframe
+          width="840"
+          height="600"
+          src="https://www.youtube.com/embed/BVDyX3M1EkU"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+      </IframeDiv>
     </div>
   );
 };
@@ -30,4 +32,11 @@ const ItemCenter = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+`;
+
+const IframeDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 20px;
+  padding-bottom: 30px;
 `;
